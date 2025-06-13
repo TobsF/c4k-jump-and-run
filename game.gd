c74f430895009c32a20d86da15cd0eb74683a579
Tobias Folie <tobias@folie.tirol>
2025-06-13 13:51:58 +0200

    added ui that displays collected treasures

--- game.gd
+++ game.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
+var treasure_count: int = 0
+const WINNING_TREASURE_COUNT: int = 3
 
 func _ready() -> void:
+	$UI.set_target(WINNING_TREASURE_COUNT)
 	for treasure in $Treasures.get_children():
 		treasure.treasure_collected.connect(_on_treasure_collected)
 
 func _on_treasure_collected() -> void:
-	print("Schatz gesammelt!")
+	treasure_count += 1
+	$UI.increase_counter()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	get_tree().call_deferred("reload_current_scene")
---
+++ ui.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+const LABEL_TEXT: String = "Treasure: %s / %s"
+var count: int = 0
+var target: int = 0
+
+func _ready() -> void:
+	_update_label()
+	
+func increase_counter() -> void:
+	count += 1
+	_update_label()
+
+func set_target(new_target: int) -> void:
+	target = new_target
+	_update_label()
+
+func _update_label() -> void:
+	$Label.text = LABEL_TEXT % [count, target]
